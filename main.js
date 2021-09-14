e635117bff7c2136e995d34272e32394a7f48da4
aliabdellatif0 <ali.abdlaitf@gmail.com>
2021-09-14 22:44:11 +0100

    testing clac ideas

--- main.js
+++ main.js
@@ -6,12 +6,27 @@ const handleButton = () => {
 }
 
 const addToScreen = (number) => {
+    //if has operand class ignore function
     const showDisplay = document.querySelector(".display")
     //add a class then remove the class in order to get the value
     
     showDisplay.innerHTML += number
 
 }
+
+let firstInput = 0
+let secondInput = 0
+const calcFunction = (numero) => {
+    
+    if (firstInput = false){
+        firstInput = numero;
+    }   else{
+        secondInput = numero;
+        }
+}
+
+console.log(firstInput)
+console.log(secondInput)
 
 ///when press equal 
 //for loop parse evaluate each token
