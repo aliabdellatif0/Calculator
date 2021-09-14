b9d43f5330d8e0766582d4267b70b59e21c67fd4
aliabdellatif0 <ali.abdlaitf@gmail.com>
2021-09-14 15:56:42 +0100

    some java functionality added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Calculator
 Approach buttons using a css grid layout 
+Buttons all laid out now; output bar at the top is done and a satisfactory colour scheme is included
---
+++ main.js
@@ -0,0 +1,14 @@
+
+const numberButtons = document.querySelectorAll("button")
+
+const handleButton = () => {
+    alert("clicked")
+}
+
+const addToScreen = () => {
+    const showDisplay = document.querySelector(".display")
+    //add a class then remove the class in order to get the value
+    const txtValue = document.querySelector(".")
+    showDisplay.innerHTML += "9"
+
+}
