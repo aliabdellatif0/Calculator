1409972df26fdd85b1a662c9e2362f85c8f7fec9
aliabdellatif0 <ali.abdlaitf@gmail.com>
2021-09-14 16:57:42 +0100

    full display with ouputs

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ const handleButton = () => {
     alert("clicked")
 }
 
-const addToScreen = () => {
+const addToScreen = (number) => {
     const showDisplay = document.querySelector(".display")
     //add a class then remove the class in order to get the value
-    const txtValue = document.querySelector(".")
-    showDisplay.innerHTML += "9"
+    
+    showDisplay.innerHTML += number
 
 }
+
+///when press equal 
+//for loop parse evaluate each token
